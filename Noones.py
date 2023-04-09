09b8da9021c74495c87067d4341399fbec578706
Alexander Akinyomola <46966054+kompleksanda@users.noreply.github.com>
2023-04-09 20:30:34 +0000

    sdf

--- Noones.py
+++ Noones.py
@@ -48,8 +48,7 @@ def initialise():
 def run():
     global token
     response = requests.post("https://api.noones.com/noones/v1/user/me", headers={'content-type': 'application/x-www-form-urlencoded', "Authorization": "Bearer {0}".format(token)})
-    print(response.json())
-    #pass
+    print(response.status_code)
     if (response.status_code == 200):
         #print(response.json())
         print("UPDATED...")
